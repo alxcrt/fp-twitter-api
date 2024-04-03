f497689ec0072b4a0ee66fef3d876fbac964a6fd
Alexandru Cretu <cretu.alexandru2000@gmail.com>
2024-04-03 23:57:26 +0300

    vercel

--- app/api/tweets/route.ts
+++ app/api/tweets/route.ts
@@ -3,6 +3,8 @@ import { getSession } from "@/lib";
 import { tweetSchema } from "@/schemas";
 import zod from "zod";
 
+export const revalidate = 0;
+
 export async function GET(req: Request, res: Response) {
   // const session = await getSession();
 
